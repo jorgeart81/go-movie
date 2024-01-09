11d70d52b418504061853dc5786203bb32ac548d
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-01-09 15:54:35 -0500

    adding a route to out handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,13 +18,13 @@ func main() {
 
 	// read from command line
 
-	// connect ot the database
+	// connect to the database
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
